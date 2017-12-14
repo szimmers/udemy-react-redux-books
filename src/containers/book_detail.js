a31efcc3a35fa35eb09134773fb9125ffc211349
Steve Zimmers <steve@soccer-1.com>
2017-12-14 10:59:26 -0500

    render book detail

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
 	render() {
+		if (!this.props.book) {
+			return <div>Select a book</div>;
+		}
+
 		return (
 			<div>
-				detail!
+				<h3>Details for</h3>
+				<div>
+					{this.props.book.title}
+				</div>
 			</div>
 		)
 	}
 }
+
+/**
+ * return will be props for component
+ * @param state
+ */
+function mapStateToProps(state) {
+	return {
+		book: state.activeBook
+	};
+}
+
+/**
+ * return will be props for BookList
+ * @param dispatch
+ * @returns {{selectBook: selectBook}|ActionCreator<any>|ActionCreatorsMapObject}
+ */
+/*
+function matchDispatchToProps(dispatch) {
+	// whenever selectBook is called, pass result to all our reducers
+	return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+*/
+
+export default connect(mapStateToProps)(BookDetail);
